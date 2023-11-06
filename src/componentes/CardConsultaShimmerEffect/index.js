f4fd16e43a30e9195f6a1ed2265b8302558fac1f
tuiza <luiza8.marlene@gmail.com>
2023-11-06 15:07:36 -0300

    feat: implenta o shimmer effect no componente de cardConsulta

---
+++ src/componentes/CardConsultaShimmerEffect/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { View, Image, } from "react-native";
+import styles from "./styles";
+import shimmerGif from '../../assets/shimmer.gif';
+
+export function CardConsultaShimmerEffect() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.pessoaArea}>
+        <Image source={shimmerGif} style={[styles.foto, {
+          overlayColor: '#FFF',
+        }]
+        } />
+        <View style={styles.informacoes}>
+          <Image source={shimmerGif} style={{ width: 200, height: 22 }} />
+
+          <View style={styles.consultaArea}>
+            <Image source={shimmerGif} style={{ width: 150, height: 20, marginTop: 3 }} />
+          </View>
+        </View>
+      </View>
+      <View style={styles.informacoesAgendamento}>
+        <Image source={shimmerGif} style={{ width: 150, height: 20 }} />
+      <View style={styles.botoesArea}>
+        <Image source={shimmerGif} style={ [styles.botaoLocalizacao, {overlayColor: '#FFF'}]} />
+      </View>
+      </View>
+    </View>
+  );
+}
--- src/telas/Principal/index.js
+++ src/telas/Principal/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, FlatList, TouchableOpacity } from "react-native";
 import { CardConsulta } from "../../componentes/CardConsulta";
 import { InformacoesUsuario } from "../../componentes/InformacoesUsuario";
@@ -6,32 +6,56 @@ import { TelaDeFundo } from "../../componentes/TelaDeFundo";
 import soniaFoto from "../../assets/Luiza.png";
 import pacientes from "./pacientes";
 import styles from "./styles";
-import Carrosel from "../../componentes/Carrosel";
+import { CardConsultaShimmerEffect } from "../../componentes/CardConsultaShimmerEffect";
 
-export default function Principal() {
+export default function Principal({ navigation }) {
   const quantidadeConsultas = pacientes.length;
+
+  const [loadingFake, setLoadingFake] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoadingFake(false)
+    }, 2000)
+
+  }, [])
+
+
+  const renderShimmer = () => {
+    const shimmerCards = [];
+    for (let i = 0; i < 4; i++) {
+      shimmerCards.push(<CardConsultaShimmerEffect key={i} />);
+    }
+    return shimmerCards;
+  }
 
   return (
     <TelaDeFundo>
-    <View style={styles.container}>
-      <InformacoesUsuario 
-        nome="Olá, Luiza"
+      <View style={styles.container}>
+        <InformacoesUsuario
+          nome="Olá, Luiza"
           detalhes={`Mais ${quantidadeConsultas} consultas hoje`}
-        foto={soniaFoto}
-      />
-      <Text style={styles.texto}>Hoje</Text>
-
-        <FlatList
-          data={pacientes}
-          keyExtractor={item => String(item.id)}
-          renderItem={({ item }) =>
-            <TouchableOpacity onPress={() => navigation.navigate("Detalhes", item)}>
-              <CardConsulta {...item} />
-            </TouchableOpacity>
-          }
-          showsVerticalScrollIndicator={false}
+          foto={soniaFoto}
         />
-    </View> 
+        <Text style={styles.texto}>Hoje</Text>
+
+
+        {loadingFake ?
+          <>
+            {renderShimmer()}
+          </> :
+          <FlatList
+            data={pacientes}
+            keyExtractor={item => String(item.id)}
+            renderItem={({ item }) =>
+              <TouchableOpacity onPress={() => navigation.navigate("Detalhes", item)}>
+                <CardConsulta {...item} />
+              </TouchableOpacity>
+            }
+            showsVerticalScrollIndicator={false}
+          />
+        }
+      </View>
     </TelaDeFundo>
   );
 }
